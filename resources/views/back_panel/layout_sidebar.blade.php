4a2edbaa44eec6a3060ac6c2c868bebf7926d110
Sirbu Ana <sirbuanca2@gmail.com>
2020-05-08 18:26:50 +0300

    Dashboard link

--- resources/views/back_panel/layout_sidebar.blade.php
+++ resources/views/back_panel/layout_sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link {{ request()->routeIs('back_panel.dashboard') ? 'active' : ""}} " href="{{ route("back_panel.dashboard") }}">
                     <span data-feather="home"></span>
                     Dashboard <span class="sr-only">(current)</span>
                 </a>
