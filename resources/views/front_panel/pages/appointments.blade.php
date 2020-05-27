66f0e6cb5c22613e4f7dae5f54963b3ca7eb3800
Sirbu Ana <sirbuanca2@gmail.com>
2020-05-27 15:10:00 +0300

    Form fix

--- resources/views/front_panel/pages/appointments.blade.php
+++ resources/views/front_panel/pages/appointments.blade.php
@@ -8,10 +8,6 @@
     {!! Form::open(['url' => route('appointment.store'), 'autocomplete'=>'off']) !!}
     @csrf
     <div class="form-group row">
-        {!! Form::label("employee_id", "Select Employee", ["class" =>"col-sm-2 col-form-label"]) !!}
-        <div class="col-sm-10">
-            {!! Form::select('employee_id',$employees, ["class"=>"form-control".($errors->has("employee_id") ? "is-invalid" : "")]) !!}
-            @error("employee_id")
         {!! Form::label("appointment[employee_id]", "Employee", ["class" =>"col-sm-2 col-form-label"]) !!}
         <div class="col-sm-4">
             {!! Form::select("appointment[employee_id]",$employees, null, ["class"=>"form-control ".($errors->has("appointment.employee_id") ? "is-invalid" : "")]) !!}
@@ -19,12 +15,8 @@
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-    </div>git 
+    </div>
     <div class="form-group row">
-        {!! Form::label("service_type_id", "Service Type", ["class" =>"col-sm-2 col-form-label"]) !!}
-        <div class="col-sm-10">
-            {!! Form::select('service_type_id',$service_types, ["class"=>"form-control".($errors->has("service_type_id") ? "is-invalid" : "")]) !!}
-            @error("service_type_id")
         {!! Form::label("appointment[service_type_id]", "Service type", ["class" =>"col-sm-2 col-form-label"]) !!}
         <div class="col-sm-4">
             {!! Form::select("appointment[service_type_id]",$service_types, null, ["class"=>"form-control ".($errors->has("appointment.service_type_id") ? "is-invalid" : "")]) !!}
@@ -44,10 +36,6 @@
         </div>
     </div>
     <div class="form-group row">
-        {!! Form::label("appointment_time", "Pick a time", ["class" =>"col-sm-2 col-form-label"]) !!}
-        <div class="col-sm-10">
-            {!! Form::select('appointment_time',$time_ranges, ["class"=>"form-control".($errors->has("appointment_time") ? "is-invalid" : "")]) !!}
-            @error("appointment_time")
         {!! Form::label("appointment[appointment_time]", "Pick a time", ["class" =>"col-sm-2 col-form-label"]) !!}
         <div class="col-sm-4">
             {!! Form::select("appointment[appointment_time]",$time_ranges, null, ["class"=>"form-control ".($errors->has("appointment.appointment_time") ? "is-invalid" : "")]) !!}
